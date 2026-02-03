793b0bb12417a713bd856a3be3ce6d5e8f319c13
A <aasveshn@gmail.com>
2026-02-03 17:55:28 +0300

    Добавлено поле - флаг является ли грань ячейки граничной

--- Face/Face.h
+++ Face/Face.h
@@ -10,7 +10,8 @@ public:
     const int Left_ID;
     const int Right_ID;
     const bool isVertical;
+    const bool isBorder;
 
-    Face(unsigned int id, int L_ID, int R_ID, bool isVert);
+    Face(unsigned int id, int L_ID, int R_ID, bool isVert, bool isBord);
 
 };
