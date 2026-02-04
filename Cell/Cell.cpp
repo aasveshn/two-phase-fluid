b4575ecb4d7d9001b64ca9d37e57b2bda31a3946
A <aasveshn@gmail.com>
2026-02-04 17:55:56 +0300

    Добавлен конструктор копирования

--- Cell/Cell.cpp
+++ Cell/Cell.cpp
@@ -1,6 +1,6 @@
 #include "Cell.h"
 
-//Cell::Cell(unsigned int id){ ID = id; }
+
 
 Cell::Cell(unsigned int id,
            unsigned int i_index,
@@ -11,12 +11,10 @@ Cell::Cell(unsigned int id,
            j(j_index),
            faces_ID{ faces_id[0], faces_id[1], faces_id[2], faces_id[3] }{}
 
-/*
-void Cell::setFaces(const int (&faces_id)[4])
-{
-    faces_ID[0] = faces_id[0]; 
-    faces_ID[1] = faces_id[1];
-    faces_ID[2] = faces_id[2];
-    faces_ID[3] = faces_id[3];
-}*/
 
+Cell::Cell(const Cell& other) 
+    : ID(other.ID), 
+      i(other.i), 
+      j(other.j), 
+      faces_ID{other.faces_ID[0], other.faces_ID[1], other.faces_ID[2], other.faces_ID[3]},
+      W(other.W){}
--- Cell/Cell.h
+++ Cell/Cell.h
@@ -10,13 +10,17 @@ class Cell{
 public:
 
     const unsigned int ID, i, j;
+    //0 - левая
+    //1 - правая
+    //2 - верхняя
+    //3 - нижняя
     const unsigned int faces_ID[4];
     
 
     StateW W;
 
-    //Cell(unsigned int ID);
+    
     Cell(unsigned int id, unsigned int i_index, unsigned int j_index, const unsigned int (&faces_id)[4]);
-
-    //void setFaces(const  int (&faces_id)[4]);
+    Cell(const Cell& other);
+    
 };
