3b9809254a39c7b7631787c698384dabbf3253dd
A <aasveshn@gmail.com>
2026-02-07 17:55:07 +0300

    Добавлен конструктор по StateW

--- States/StateU.h
+++ States/StateU.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include "StateW.h"
+#include "../EOS/EOS.h"
+#include "../Components/Components.h"
+
 struct StateU {
     // Используем union для доступа и по именам, и по индексу
     union {
@@ -15,6 +19,20 @@ struct StateU {
     
     StateU(double ar1, double aru1, double arE1, double ar2, double aru2, double arE2)
         : ar1(ar1), aru1(aru1), arE1(arE1), ar2(2), aru2(aru2), arE2(arE2) {}
+
+    StateU(const StateW& w, const Components& comp)
+    {
+        ar1 = w.a1*w.ro1;
+        aru1 = w.a1*w.ro1*w.u1;
+        arE1 = w.a1*w.ro1*E(e_P_ro(w.P1, w.ro1, comp.p1), w.u1);
+        ar2 = (1-w.a1)*w.ro2;
+        aru2 = (1-w.a1)*w.ro2*w.u2;
+        arE2 =(1-w.a1)*w.ro2*E(e_P_ro(w.P2, w.ro2, comp.p2), w.u2);
+    }
+
+    inline void zero() {
+        ar1 = aru1 = arE1 = ar2 = aru2 = arE2 = 0.0;
+    }
 
     
     inline StateU operator+(const StateU& u) const {
