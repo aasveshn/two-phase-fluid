eeef660abacd1330babc6e075b225096dde0fda0
A <aasveshn@gmail.com>
2026-02-09 20:15:50 +0300

    Небольшие изменения

--- Constants/Mesh_Constants.h
+++ Constants/Mesh_Constants.h
@@ -1,4 +1,9 @@
+//CFL
+const double CFL = 0.1;
+// cell constants
 const double dx = 0.01;
 const double dy = 0.01;
-
+//Slope limiter
 const double betta = 1.0;
+//final time 
+const double T = 0.004;
