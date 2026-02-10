6bca7b97797aa86ce874d5c78acf9a4c8f3151bf
A <aasveshn@gmail.com>
2026-02-10 19:27:56 +0300

    Исправление ошибок

--- RiemanSolver/RiemanSolver.cpp
+++ RiemanSolver/RiemanSolver.cpp
@@ -1,6 +1,6 @@
 #include "RiemanSolver.h"
 
-RiemanSolver::RiemanSolver(unsigned int N, unsigned int k): Flux_X(k), Flux_Y(N-k){};
+RiemanSolver::RiemanSolver(unsigned int N, unsigned int k): Flux_X(k+1), Flux_Y(N-k){};
 
 void RiemanSolver::HLLC(const Mesh& mesh, const std::vector<StateW>& WL_in, const std::vector<StateW>& WR_in,  
                         unsigned int H_idx, bool is_X_dir, const Components& phases)
@@ -8,15 +8,17 @@ void RiemanSolver::HLLC(const Mesh& mesh, const std::vector<StateW>& WL_in, cons
     
     unsigned int N, interval_L, interval_R;
     unsigned int face_dirs[2];
+    int k;
     std::vector<StateU>& flux = is_X_dir ? Flux_X : Flux_Y;
 
     if(is_X_dir)
     {
         interval_L = 0;
         interval_R = H_idx -1;
-        N = H_idx;
+        N = H_idx+1;
         face_dirs[0] = 0;
         face_dirs[1] = 1;
+        k=0;
     }
     else
     {
@@ -25,6 +27,7 @@ void RiemanSolver::HLLC(const Mesh& mesh, const std::vector<StateW>& WL_in, cons
         N = mesh.Faces.size() - H_idx;
         face_dirs[0] = 2;
         face_dirs[1] = 3;
+        k = H_idx;
     }
 
     int idx_L, idx_R;
@@ -38,12 +41,17 @@ void RiemanSolver::HLLC(const Mesh& mesh, const std::vector<StateW>& WL_in, cons
         
         idx_L = mesh.Faces[i].Left_ID;
         idx_R = mesh.Faces[i].Right_ID;
-
-        if(idx_L == -1 || idx_R == -1)
+ 
+        
+        if(idx_L == -1)
         {    
-            flux[i].zero();
-            continue;
+            idx_L = idx_R;
         }
+        else if(idx_R == -1)
+        {
+            idx_R = idx_L;
+        }
+        
 
         const StateW& WL = WR_in[idx_L];
         const StateW& WR = WL_in[idx_R];
@@ -99,39 +107,39 @@ void RiemanSolver::HLLC(const Mesh& mesh, const std::vector<StateW>& WL_in, cons
 
         if(sL >= 0)
         {
-            flux[i][0] = UL[1];
-            flux[i][1] = UL[1]*WL.u1 + WL.a1*WL.P1;
-            flux[i][2] = WL.a1*(WL.ro1*(UL[2]/UL[0]) + WL.P1)*WL.u1;
-            flux[i][3] = UL[4];
-            flux[i][4] = UL[4]*WL.u2 + a2L*WL.P2;
-            flux[i][5] = a2L*(WL.ro2*(UL[5]/UL[3]) + WL.P2)*WL.u2;
+            flux[i-k][0] = UL[1];
+            flux[i-k][1] = UL[1]*WL.u1 + WL.a1*WL.P1;
+            flux[i-k][2] = WL.a1*(WL.ro1*(UL[2]/UL[0]) + WL.P1)*WL.u1;
+            flux[i-k][3] = UL[4];
+            flux[i-k][4] = UL[4]*WL.u2 + a2L*WL.P2;
+            flux[i-k][5] = a2L*(WL.ro2*(UL[5]/UL[3]) + WL.P2)*WL.u2;
         }
         else if(sL <= 0 && s_C >= 0)
         {
-            flux[i][0] = UL[1] + sL*(UL_C[0] - UL[0]);
-            flux[i][1] = UL[1] * WL.u1 + WL.a1*WL.P1 + sL*(UL_C[1] - UL[1]);
-            flux[i][2] = WL.a1 * (WL.ro1*(UL[2]/UL[0]) + WL.P1) * WL.u1 + sL*(UL_C[2] - UL[2]);
-            flux[i][3] = UL[4] + sL*(UL_C[3] - UL[3]);
-            flux[i][4] = UL[4] * WL.u2 + a2L*WL.P2 + sL*(UL_C[4] - UL[4]);
-            flux[i][5] = a2L * (WL.ro2*(UL[5]/UL[3]) + WL.P2) * WL.u2 + sL*(UL_C[5] - UL[5]);
+            flux[i-k][0] = UL[1] + sL*(UL_C[0] - UL[0]);
+            flux[i-k][1] = UL[1] * WL.u1 + WL.a1*WL.P1 + sL*(UL_C[1] - UL[1]);
+            flux[i-k][2] = WL.a1 * (WL.ro1*(UL[2]/UL[0]) + WL.P1) * WL.u1 + sL*(UL_C[2] - UL[2]);
+            flux[i-k][3] = UL[4] + sL*(UL_C[3] - UL[3]);
+            flux[i-k][4] = UL[4] * WL.u2 + a2L*WL.P2 + sL*(UL_C[4] - UL[4]);
+            flux[i-k][5] = a2L * (WL.ro2*(UL[5]/UL[3]) + WL.P2) * WL.u2 + sL*(UL_C[5] - UL[5]);
         }
         else if(s_C <= 0 && sR >= 0)
         {
-            flux[i][0] = UR[1] + sR*(UR_C[0] - UR[0]);
-            flux[i][1] = UR[1] * WR.u1 + WR.a1*WR.P1 + sR*(UR_C[1] - UR[1]);
-            flux[i][2] = WR.a1 * (WR.ro1*(UR[2]/UR[0]) + WR.P1) * WR.u1 + sR*(UR_C[2] - UR[2]);
-            flux[i][3] = UR[4] + sR*(UR_C[3] - UR[3]);
-            flux[i][4] = UR[4] * WR.u2 + a2R*WR.P2 + sR*(UR_C[4] - UR[4]);
-            flux[i][5] = a2R * (WR.ro2*(UR[5]/UR[3]) + WR.P2) * WR.u2 + sR*(UR_C[5] - UR[5]);
+            flux[i-k][0] = UR[1] + sR*(UR_C[0] - UR[0]);
+            flux[i-k][1] = UR[1] * WR.u1 + WR.a1*WR.P1 + sR*(UR_C[1] - UR[1]);
+            flux[i-k][2] = WR.a1 * (WR.ro1*(UR[2]/UR[0]) + WR.P1) * WR.u1 + sR*(UR_C[2] - UR[2]);
+            flux[i-k][3] = UR[4] + sR*(UR_C[3] - UR[3]);
+            flux[i-k][4] = UR[4] * WR.u2 + a2R*WR.P2 + sR*(UR_C[4] - UR[4]);
+            flux[i-k][5] = a2R * (WR.ro2*(UR[5]/UR[3]) + WR.P2) * WR.u2 + sR*(UR_C[5] - UR[5]);
         }
         else if(sR <= 0)
         {
-            flux[i][0] = UR[1];
-            flux[i][1] = UR[1]*WR.u1 + WR.a1*WR.P1;
-            flux[i][2] = WR.a1*(WR.ro1*(UR[2]/UR[0]) + WR.P1)*WR.u1;
-            flux[i][3] = UR[4];
-            flux[i][4] = UR[4]*WR.u2 + a2R*WR.P2;
-            flux[i][5] = a2R*(WR.ro2*(UR[5]/UR[3]) + WR.P2)*WR.u2;
+            flux[i-k][0] = UR[1];
+            flux[i-k][1] = UR[1]*WR.u1 + WR.a1*WR.P1;
+            flux[i-k][2] = WR.a1*(WR.ro1*(UR[2]/UR[0]) + WR.P1)*WR.u1;
+            flux[i-k][3] = UR[4];
+            flux[i-k][4] = UR[4]*WR.u2 + a2R*WR.P2;
+            flux[i-k][5] = a2R*(WR.ro2*(UR[5]/UR[3]) + WR.P2)*WR.u2;
         }
         else
         {
