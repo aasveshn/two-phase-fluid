9d7e990eb32c187175c404301d94a20dfca209da
A <aasveshn@gmail.com>
2026-02-10 19:26:43 +0300

    Изменены значения для тестового запуска решения Гиперболической части системы

--- Constants/Physics_Constants.h
+++ Constants/Physics_Constants.h
@@ -7,12 +7,20 @@ const double Cp_water_vapor = 1487.0;
 const double q_water_vapor = 2030000.0;
 const double qs_water_vapor = -23000.0;
 
+const double gamma_water_liquid = 1.430;
+const double P0_water_liquid = 0.0;
+const double Cv_water_liquid = 1040.0;
+const double Cp_water_liquid = 1487.0;
+const double q_water_liquid = 2030000.0;
+const double qs_water_liquid = -23000.0;
+
 //water liquid phase
+/*
 const double gamma_water_liquid = 2.350;
 const double P0_water_liquid = 1000000000.0;
 const double Cv_water_liquid = 1816.0;
 const double Cp_water_liquid = 4267.0;
 const double q_water_liquid= -1167000.0;
-const double qs_water_liquid = 0.0;
+const double qs_water_liquid = 0.0;*/
 
 
