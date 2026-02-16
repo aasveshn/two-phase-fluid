4cbc337bbfaaa7adba576e35c69812b7f36b8ff7
A <aasveshn@gmail.com>
2026-02-16 12:16:45 +0300

    Добавленны функции для векторов потоков

--- States/StateU.h
+++ States/StateU.h
@@ -26,11 +26,11 @@ struct StateU {
         ar1 = w.a1*w.ro1;
         aru1 = w.a1*w.ro1*w.u1;
         arv1 = w.a1*w.ro1*w.v1;
-        arE1 = w.a1*w.ro1*E(e_P_ro(w.P1, w.ro1, comp.p1), w.u1);
+        arE1 = w.a1*w.ro1*E(e_P_ro(w.P1, w.ro1, comp.p1), w.u1, w.v1);
         ar2 = (1-w.a1)*w.ro2;
         aru2 = (1-w.a1)*w.ro2*w.u2;
         arv2 = (1-w.a1)*w.ro2*w.v2;
-        arE2 =(1-w.a1)*w.ro2*E(e_P_ro(w.P2, w.ro2, comp.p2), w.u2);
+        arE2 =(1-w.a1)*w.ro2*E(e_P_ro(w.P2, w.ro2, comp.p2), w.u2, w.v2);
     }
 
     inline void zero() {
@@ -65,3 +65,36 @@ struct StateU {
     inline double operator[](int i) const { return v[i]; }
     inline double& operator[](int i) { return v[i]; }
 };
+
+inline StateU get_F_X(const StateU& U, const StateW& W, double a1, double a2) {
+    StateU F;
+    
+    F[0] = U[1];                          
+    F[1] = U[1] * W.u1 + a1 * W.P1;       
+    F[2] = U[1] * W.v1;                   
+    F[3] = (U[3] + a1 * W.P1) * W.u1;     
+
+    F[4] = U[5];                         
+    F[5] = U[5] * W.u2 + a2 * W.P2;       
+    F[6] = U[5] * W.v2;                  
+    F[7] = (U[7] + a2 * W.P2) * W.u2;     
+
+    return F;
+}
+
+
+inline StateU get_F_Y(const StateU& U, const StateW& W, double a1, double a2) {
+    StateU G;
+
+    G[0] = U[2];                          
+    G[1] = U[2] * W.u1;                   
+    G[2] = U[2] * W.v1 + a1 * W.P1;       
+    G[3] = (U[3] + a1 * W.P1) * W.v1;     
+
+    G[4] = U[6];                         
+    G[5] = U[6] * W.u2;                   
+    G[6] = U[6] * W.v2 + a2 * W.P2;       
+    G[7] = (U[7] + a2 * W.P2) * W.v2;     
+
+    return G;
+}
