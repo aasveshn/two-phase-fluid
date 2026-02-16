98432140cb78e267013fdc0a06ab4f22a84750d0
A <aasveshn@gmail.com>
2026-02-16 12:21:39 +0300

    Внесены изменения с учетом второй компоненты скорости

--- Solver/Solver.cpp
+++ Solver/Solver.cpp
@@ -15,48 +15,101 @@ Solver::Solver(Mesh& msh):mesh(msh),
                                        qs_water_liquid)), 
                            HyperbolicOp(msh, phases) {}
 
-double  Solver::compute_dt()
+double Solver::compute_dt()
 {
-    std::vector<double> EightValues(7);
+    double MaxValue = -1.0; 
     double h = std::max(dx, dy);
-    double MaxEihgtValue = -1;
-    for(int i = 0; i < mesh.Cells.size(); ++i)
+
+    for (int i = 0; i < mesh.Cells.size(); ++i)
     {
-        StateW& W = mesh.Cells[i].W;
+        const StateW& W = mesh.Cells[i].W;
+        
         double C1 = f_C(W.P1, W.ro1, phases.p1);
         double C2 = f_C(W.P2, W.ro2, phases.p2);
-        double UI = f_UI(W);
+        
+        double UIx = f_UI_x(W);
+        double UIy = f_UI_y(W);
 
-        EightValues[0] = UI;
-        EightValues[1] = W.u1 - C1;
-        EightValues[2] = W.u1;
-        EightValues[3] = W.u1 + C1;
-        EightValues[4] = W.u2 - C2;
-        EightValues[5] = W.u2;
-        EightValues[6] = W.u2 + C2;
+     
+        double cellMax = std::max({
+            
+            std::abs(UIx),              
+            std::abs(W.u1 - C1),       
+            std::abs(W.u1),             
+            std::abs(W.u1),             
+            std::abs(W.u1 + C1),       
+            std::abs(W.u2 - C2),      
+            std::abs(W.u2),            
+            std::abs(W.u2),             
+            std::abs(W.u2 + C2),        
 
-        for(int j = 0; j < 7; ++j)
-            MaxEihgtValue = std::max(MaxEihgtValue, std::fabs(EightValues[j]));
+           
+            std::abs(UIy),              
+            std::abs(W.v1 - C1),       
+            std::abs(W.v1),            
+            std::abs(W.v1),            
+            std::abs(W.v1 + C1),        
+            std::abs(W.v2 - C2),       
+            std::abs(W.v2),             
+            std::abs(W.v2),             
+            std::abs(W.v2 + C2)         
+        });
 
+        if (cellMax > MaxValue) MaxValue = cellMax;
     }
 
-    return CFL * h / MaxEihgtValue;
+    return CFL * h / MaxValue;
 }
 
 void Solver::Solve()
 {
+    //Делегировать запись в файлы в отедльный метод
     double time = 0.0;
     double dt = 1e-10;
     int step = 0;
-    
+
+    std::ofstream f_a1("output/a1.txt");
+    std::ofstream f_ro1("output/ro1.txt");
+    std::ofstream f_u1("output/u1.txt");
+    std::ofstream f_v1("output/v1.txt");
+    std::ofstream f_P1("output/P1.txt");
+    std::ofstream f_ro2("output/ro2.txt");
+    std::ofstream f_u2("output/u2.txt");
+    std::ofstream f_v2("output/v2.txt");
+    std::ofstream f_P2("output/P2.txt");
+
+     if (!f_a1.is_open()) {
+        std::cerr << "Error opening output files!" << std::endl;
+        return;
+    }
+
+   
    //1500   
     while(time < T)
     {
         HyperbolicOp.HyperbolicStep(dt);
         time += dt;
         dt = compute_dt();
+        std::cout<<time<<"\n";
         ++step;
-        if(step == 1)
+        if(step == 50)
             break;
     }
+
+
+     for (const auto& cell : mesh.Cells) {
+        f_a1  << cell.W.a1  << "\n";
+        f_ro1 << cell.W.ro1 << "\n";
+        f_u1  << cell.W.u1  << "\n";
+        f_v1  << cell.W.v1  << "\n";
+        f_P1  << cell.W.P1  << "\n";
+        f_ro2 << cell.W.ro2 << "\n";
+        f_u2  << cell.W.u2  << "\n";
+        f_v2  << cell.W.v2  << "\n";
+        f_P2  << cell.W.P2  << "\n";
+    }
+
+    f_a1.close(); f_ro1.close(); f_u1.close(); f_v1.close(); f_P1.close();
+    f_ro2.close(); f_u2.close(); f_v2.close(); f_P2.close();
+    
 }
