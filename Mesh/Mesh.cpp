7aec1959e7bcde9546be469db9fb937b9bb50653
A <aasveshn@gmail.com>
2026-02-19 17:48:31 +0300

    Изменение параметров начальной инициализации

--- Mesh/Mesh.cpp
+++ Mesh/Mesh.cpp
@@ -54,12 +54,12 @@ Mesh::Mesh(std::string filename)
 //Необходима реализация
 void Mesh::SetInitialCondidions()
 {
-    unsigned int discontinuity = 600; //100
+    unsigned int discontinuity = 750; //100
     double a1L, ro1L, u1L, v1L, P1L, ro2L, u2L, v2L, P2L,
            a1R, ro1R, u1R, v1R, P1R, ro2R, u2R, v2R, P2R;
     double WL[7];
 //Сделать чтение из файла?
-    a1L = 1e-6;
+    a1L = 1e-8;
     ro1L = 2;
     u1L = 0;
     v1L = 0;
@@ -69,7 +69,7 @@ void Mesh::SetInitialCondidions()
     v2L = 0;
     P2L = 100000000.0;
 
-    a1R = 1-1e-6;
+    a1R = 1-1e-8;
     ro1R = 2;
     u1R = 0;
     v1R = 0;
